0db5b4def798b717b4ed90d3cab365103d65a44c
Jonathan Arnett <jonarnett90@gmail.com>
2014-05-20 16:03:00 -0400

    Migrated database and made ultra-simple project list view

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140124015319) do
+ActiveRecord::Schema.define(version: 20140520192606) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,13 @@ ActiveRecord::Schema.define(version: 20140124015319) do
     t.integer  "meeting_id"
     t.integer  "talk_id"
     t.integer  "speaker_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "projects", force: true do |t|
+    t.string   "title"
+    t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
