80ef8b04938dce059b94a77dd6fd889ba35156aa
Jonathan Arnett <jonarnett90@gmail.com>
2014-09-24 14:06:02 -0400

    First go at dates auto-updating

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,12 @@
 class StaticPagesController < ApplicationController
   def home
+    ## Next meeting date logic
+    wday = 3  # Meetings are on Wednesdays
+    today = Date.today
+    this_wednesday = today.beginning_of_week.next_day(wday - 1)
+    next_wednesday = today.next_week.next_day(wday - 1)
+
+    @next_date = today.wday <= wday ? this_wednesday : next_wednesday
   end
 
   def about
