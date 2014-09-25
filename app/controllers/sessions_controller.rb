02bd4a474ff07d8226c8ff9fc3fbc1ff6f780b3f
Mark Tareshawty <tarebyte@gmail.com>
2014-09-25 11:59:21 -0400

    fixed gravatar_id api depreciation and authentication with omniauth

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,9 +2,10 @@ class SessionsController < ApplicationController
 
   # TODO: Redirect to back from whatever page they were at
   def create
-    user = User.from_omniauth(env['omniauth.auth'])
+    auth = request.env["omniauth.auth"]
+    user = User.find_by_provider_and_uid(auth["provider"], auth["uid"]) || User.create_with_omniauth(auth)
     session[:user_id] = user.id
-    redirect_to root_url, notice: "Signed in."
+    redirect_to root_url, notice: "Signed In!"
   end
 
   def destroy
