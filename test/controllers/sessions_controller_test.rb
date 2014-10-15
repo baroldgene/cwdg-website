4dc3d8e881b987aeefd1070887f82b5e4fda2cfe
Mark Tareshawty <tarebyte@gmail.com>
2014-10-15 02:38:28 -0400

    WIP

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -1,4 +1,28 @@
 require 'test_helper'
 
 class SessionsControllerTest < ActionController::TestCase
+
+  setup do
+    request.env['omniauth.auth'] = OmniAuth.config.mock_auth[:github]
+  end
+
+  test 'it should create a user' do
+
+  end
+
+  test 'it should create a session' do
+
+  end
+
+  test 'it should redirect the user to the user\'s personal url' do
+
+  end
+
+  test 'it should clear the session' do
+
+  end
+
+  test 'it should redirect to the homepage' do
+
+  end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,6 +4,18 @@ require 'rails/test_help'
 require "minitest/reporters"
 Minitest::Reporters.use!
 
+OmniAuth.config.test_mode = true
+omniauth_hash = { 'provider' => 'github',
+                  'uid' => '12345',
+                  'info' => {
+                      'name'     => 'Mark Tareshawty',
+                      'email'    => 'tarebyte@gmail.com',
+                      'nickname' => 'tarebyte',
+                      'image'    => 'https://avatars.githubusercontent.com/u/564113?v=2'
+                  }}
+
+OmniAuth.config.add_mock(:github, omniauth_hash)
+
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
